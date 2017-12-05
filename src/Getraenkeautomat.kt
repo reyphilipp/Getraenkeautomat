5efb3e166a2b43a942eb639076c52de26c726cb4
reyphilipp <reyphilipp@freenet.de>
2017-12-05 20:17:54 +0100

    Fertig!

--- src/Getraenkeautomat.kt
+++ src/Getraenkeautomat.kt
@@ -1,43 +1,83 @@
-/**
- * Created by admin on 04.12.2017.
- */
+import kotlin.system.exitProcess
+
 data class Getraenk(val getraenk: String, val preis: Double)
-val getraenkeliste = arrayListOf<Getraenk>(Getraenk("Wasser", 0.5),Getraenk("Limonade", 1.0),Getraenk("Bier", 2.0))
+
+val getraenkeliste = arrayListOf<Getraenk>(Getraenk("Wasser", 0.5), Getraenk("Limonade", 1.0), Getraenk("Bier", 2.0))
+var auswahl = 0
+
 
 fun main(args: Array<String>) {
-    startBildschirm()
-    println(bezahlen(auswahl()))
 
+    startBildschirm()
+    auswahl = auswaehlen()
+    rechnen()
 }
 
 fun startBildschirm() {
     println("Wähle ein Getränk:\n")
     for (i in getraenkeliste.indices)
-    println(i.toString() + ")${getraenkeliste[i].getraenk}(%.2f".format(getraenkeliste[i].preis)+" CHF)")
+        println(i.toString() + ")${getraenkeliste[i].getraenk}(%.2f".format(getraenkeliste[i].preis) + " CHF)")
 }
 
-fun auswahl(): Int{
-    println("Wählen sie zwischen "+getraenkeliste.indices+" aus!")
+fun auswaehlen(): Int {
+    println("Wählen sie zwischen " + getraenkeliste.indices + " aus!")
     val input = readLine()
-    try{
+    try {
         if (input != null) {
             return input.toInt()
         }
-    } catch (e: Exception){
+    } catch (e: Exception) {
         println(e)
+        exitProcess(0)
     }
     return 99
 }
 
-fun bezahlen(auswahl: Int):Double{
-    println("Wirf"+getraenkeliste[auswahl].preis+" CHF ein")
+fun aufforderungBezahlen(Betrag: Double) {
+    println("Wirf %.2f".format(Betrag) + " CHF ein")
+}
+
+fun muenzEinwurf(): Double {
     val input = readLine()
-    try{
+    try {
         if (input != null) {
-            input + 0
+            return input.toDouble()
         }
-    } catch (e: Exception){
+    } catch (e: Exception) {
         println(e)
+        exitProcess(0)
     }
     return 99.0
+}
+
+fun rechnen() {
+    var istBetrag = 0.0
+    var sollBetrag = (getraenkeliste[auswahl].preis)
+    aufforderungBezahlen(sollBetrag)
+    istBetrag = muenzEinwurf()
+
+    while (istBetrag < sollBetrag) {
+        aufforderungBezahlen(rundeBetrag(sollBetrag - istBetrag))
+        sollBetrag = sollBetrag - istBetrag
+        istBetrag = muenzEinwurf()
+    }
+    if (istBetrag == sollBetrag) {
+        getraenkeAsugabe()
+    }
+    if (istBetrag > sollBetrag) {
+        getraenkeAsugabe()
+        rueckgeld(istBetrag, sollBetrag)
+    }
+}
+
+fun rueckgeld(istBetrag: Double, sollBetrag: Double) {
+    println("%.2f".format(istBetrag - sollBetrag) + " CHF Rückgeld wird ausgegeben")
+}
+
+fun rundeBetrag(betrag: Double): Double {
+    return Math.round(betrag * 100.0) / 100.0
+}
+
+fun getraenkeAsugabe() {
+    println("Getränk " + getraenkeliste[auswahl].getraenk + " wird ausgegeben")
 }
